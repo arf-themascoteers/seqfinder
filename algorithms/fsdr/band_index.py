468b23ab126e71f8a2f869ca588061abd1204f82
arif <arif@themascoteers.com>
2023-12-24 14:02:44 +1100

    done

--- algorithms/fsdr/band_index.py
+++ algorithms/fsdr/band_index.py
@@ -13,37 +13,34 @@ class BandIndex(nn.Module):
         self.original_feature_size = original_feature_size
         self.seq = seq
         self.mode = mode
-        self.sequence_length = 5
         self.distance = distance
         self.normalized_distance = self.distance/original_feature_size
         self.seq_size = seq_size
         if seq:
             if self.mode == "linear_multi":
                 self.linear = nn.Sequential(
-                    nn.Linear(self.sequence_length,5),
-                    nn.LeakyReLU(),
-                    nn.Linear(5,1)
+                    nn.Linear(self.seq_size,self.seq_size)
                 )
-                self.distance_vector = torch.full((seq_size,), self.normalized_distance) * torch.linspace(0,self.seq_size-1,self.seq_size)
+        else:
+            self.seq_size = 1
+        self.distance_vector = torch.full((seq_size,), self.normalized_distance) * torch.linspace(0,self.seq_size-1,self.seq_size)
 
     def forward(self, spline):
         main_index = self.index_value()
+        indices = self.distance_vector + main_index
         if self.seq:
             if self.mode == "linear_multi":
-                return self.forward_fsdr_seq_linear_multi(spline, main_index)
+                return self.forward_fsdr_seq_linear_multi(spline, indices)
         else:
-            return self.forward_fsdr(spline, main_index)
+            return self.forward_fsdr(spline, indices)
 
-    def forward_fsdr(self, spline, main_index):
-        return spline.evaluate(main_index)
+    def forward_fsdr(self, spline, indices):
+        return spline.evaluate(indices).reshape(-1)
 
-    def forward_fsdr_seq_linear_multi(self, spline, main_index):
-        indices = None
-        for i in range(self.sequence_length):
-            idxs = torch.cat([idx + (i * self.normalized_distance)], dim=0)
-            outs = spline.evaluate(idxs)
-            outs = outs.permute(1, 0)
-            return self.linear(outs).reshape(-1)
+    def forward_fsdr_seq_linear_multi(self, spline, indices):
+        band_values = spline.evaluate(indices)
+        band_values = band_values.permute(1, 0)
+        return self.linear(band_values).reshape(-1)
 
     def index_value(self):
         return F.sigmoid(self.raw_index)
