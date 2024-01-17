c72cb07066eda075e9f7a0f29bbe83fe7871af9f
Arif <arif@themascoteers.com>
2024-01-18 03:10:31 +1100

    done

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 from evaluator import Evaluator
 
-def fsdr_confs(seq_sizes, distances, distance_learnabilities, embedding_methods, embedding_sizes, outs):
-    for seq_size in seq_sizes:
-        for distance in distances:
-            for distance_learnability in distance_learnabilities:
-                for embedding_method in embedding_methods:
-                    for embedding_size in embedding_sizes:
-                        for out in outs:
-                            yield seq_size, distance, distance_learnability, embedding_method, embedding_size, out
+def confs(algorithms, features, samples, 
+          filters, seq_sizes,  distances,
+          distance_learnabilities, embedding_methods, embedding_sizes,
+          outs):
+    for algorithm in algorithms:
+        for feature in features:
+            for sample in samples:
+                for filter in filters:
+                    for seq_size in seq_sizes:
+                        for distance in distances:
+                            for distance_learnability in distance_learnabilities:
+                                for embedding_method in embedding_methods:
+                                    for embedding_size in embedding_sizes:
+                                        for out in outs:
+                                            yield\
+                                                algorithm,\
+                                                feature,\
+                                                sample,\
+                                                filter,\
+                                                seq_size,\
+                                                distance,\
+                                                distance_learnability,\
+                                                embedding_method,\
+                                                embedding_size,\
+                                                out
 
 if __name__ == '__main__':
     algorithms = ["fsdr"]
     features = [66]
     samples = [871]
-    sizes = [5]
-
+    filters = [5]
     seq_sizes = [1]
     distances = [0]
     distance_learnabilities = [None]  # "none"(None),"uniform","diverse"
@@ -23,20 +39,24 @@ if __name__ == '__main__':
     outs = ["output"]  # output, concat_input, concat_mid_layer
 
     tasks = []
-    for feature in features:
-        for sample in samples:
-                for size in sizes:
-                    for algorithm in algorithms:
-                        if algorithm == "fsdr":
-                            for seq_size in seq_sizes:
-                                for seq_size, distance, distance_learnability, embedding_method, embedding_size, out in fsdr_confs(seq_sizes, distances, distance_learnabilities, embedding_methods, embedding_sizes, outs)
-                            tasks.append(
-                                {
-                                    "feature": feature,
-                                    "sample": sample,
-                                    "target_feature_size": size,
-                                    "algorithm": algorithm
-                                }
-                            )
+    for algorithm, feature, sample, filter, seq_size, \
+            distance, distance_learnability, embedding_method, embedding_size, out \
+            in \
+            confs(algorithms, features, samples, filters, seq_sizes,  
+                  distances, distance_learnabilities, embedding_methods, embedding_sizes, outs):
+        tasks.append(
+            {
+                "algorithm": algorithm,
+                "feature": feature,
+                "sample": sample,
+                "filter": filter,
+                "seq_size": seq_size,
+                "distance": distance,
+                "distance_learnability": distance_learnability,
+                "embedding_method": embedding_method,
+                "embedding_size": embedding_size,
+                "out": out
+            }
+        )
     ev = Evaluator(tasks)
     ev.evaluate()
